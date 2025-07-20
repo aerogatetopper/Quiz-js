6840f991f0a46feecb7fb14413bd46df0d9f51c0
aerogatetopper <148571290+aerogatetopper@users.noreply.github.com>
2025-07-20 12:00:53 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,3 +1,11 @@
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        MathJax.typeset();
+    });
+</script>
 <script>
     MathJax = {
         tex: {
